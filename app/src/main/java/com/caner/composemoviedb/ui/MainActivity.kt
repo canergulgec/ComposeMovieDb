1519fa7ed10ca748690cbc58d03727a97f33b00f
Caner Gülgeç <canergulgec@gmail.com>
2021-07-15 21:00:10 +0300

    Set status bar color using accompanist SystemUiController

--- app/src/main/java/com/caner/composemoviedb/ui/MainActivity.kt
+++ app/src/main/java/com/caner/composemoviedb/ui/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.rememberNavController
 import com.caner.composemoviedb.MovieApp
 import com.caner.composemoviedb.ui.screen.BottomNavigationBar
 import com.caner.composemoviedb.ui.screen.FloatingButton
 import com.caner.composemoviedb.ui.screen.Navigation
 import com.caner.composemoviedb.ui.theme.MovieItemComposeTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
@@ -30,6 +32,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MovieItemComposeTheme(darkTheme = app.isDark.value) {
+                val systemUiController = rememberSystemUiController()
+                SideEffect {
+                    systemUiController.setStatusBarColor(
+                        color = if (app.isDark.value) {
+                            Color.DarkGray
+                        } else {
+                            Color.LightGray
+                        },
+                        darkIcons = true
+                    )
+                }
+
                 val navController = rememberNavController()
                 Scaffold(
                     bottomBar = { BottomNavigationBar(navController) },
