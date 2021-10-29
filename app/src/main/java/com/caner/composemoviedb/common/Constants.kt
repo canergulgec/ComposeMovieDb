948b0e9cffebee1f2a1a342d5c13b74715a2d74f
Caner Gülgeç <canergulgec@gmail.com>
2021-10-29 12:53:20 +0300

    Updated MoviesPagingSource

--- app/src/main/java/com/caner/composemoviedb/common/Constants.kt
+++ app/src/main/java/com/caner/composemoviedb/common/Constants.kt
@@ -6,6 +6,7 @@ object Constants {
     const val PAGE = "page"
     const val NOW_PLAYING_MOVIES = 1
     const val UPCOMING_MOVIES = 2
+    const val MOVIE_STARTING_PAGE_INDEX = 1
 
     const val PREF_KEY = "ui_mode_pref"
     const val DARK_MODE = "dark_mode"
--- app/src/main/java/com/caner/composemoviedb/domain/paging/MoviesPagingSource.kt
+++ app/src/main/java/com/caner/composemoviedb/domain/paging/MoviesPagingSource.kt
@@ -15,7 +15,7 @@ class MoviesPagingSource @Inject constructor(
 ) : PagingSource<Int, Movie>() {
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Movie> {
-        val page = params.key ?: 1
+        val page = params.key ?: Constants.MOVIE_STARTING_PAGE_INDEX
 
         return try {
             val apiRequest = if (type == Constants.NOW_PLAYING_MOVIES) {
@@ -27,7 +27,7 @@ class MoviesPagingSource @Inject constructor(
                 val data = movieMapper.to(this)
                 LoadResult.Page(
                     data = data.movies,
-                    prevKey = null,
+                    prevKey = if (page == Constants.MOVIE_STARTING_PAGE_INDEX) null else page - 1,
                     nextKey = if (page == this.total) null else page + 1
                 )
             }
@@ -45,6 +45,9 @@ class MoviesPagingSource @Inject constructor(
     }
 
     override fun getRefreshKey(state: PagingState<Int, Movie>): Int? {
-        return state.anchorPosition
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
     }
 }
