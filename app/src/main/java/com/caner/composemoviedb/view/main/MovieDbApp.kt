7a1d4c37317a3fe3f14cb480eaa75bc51db55726
canergulgec <caner.gulgec@ozu.edu.tr>
2022-02-19 12:14:07 +0300

    Bump dependencies to latest stable version

--- app/src/main/java/com/caner/composemoviedb/view/main/MovieDbApp.kt
+++ app/src/main/java/com/caner/composemoviedb/view/main/MovieDbApp.kt
@@ -37,16 +37,10 @@ fun MovieDbApp(changeTheme: () -> Unit, viewModel: MainViewModel = hiltViewModel
                     controller = navController,
                     onNavigationSelected = { screen ->
                         navController.navigate(screen.route) {
-                            // Pop up to the start destination of the graph to
-                            // avoid building up a large stack of destinations
-                            // on the back stack as users select items
                             popUpTo(navController.graph.findStartDestination().id) {
                                 saveState = true
                             }
-                            // Avoid multiple copies of the same destination when
-                            // reselect the same item
                             launchSingleTop = true
-                            // Restore state when reselect a previously selected item
                             restoreState = true
                         }
                     },
