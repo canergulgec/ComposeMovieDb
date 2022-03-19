8fed77a68dd33628681f514871e2cc5ea882de6c
canergulgec <caner.gulgec@ozu.edu.tr>
2022-03-19 14:11:32 +0300

    Move PopularMoviesHorizontalPager function in PopularMovies compose function

--- app/src/main/java/com/caner/composemoviedb/view/movie/MovieRoute.kt
+++ app/src/main/java/com/caner/composemoviedb/view/movie/MovieRoute.kt
@@ -162,67 +162,63 @@ fun PopularMovies(uiState: MovieUiState) {
             style = MaterialTheme.typography.h6
         )
 
-        PopularMoviesHorizontalPager(uiState.popularMovies)
-    }
-}
+        HorizontalPager(
+            count = uiState.popularMovies.size,
+            contentPadding = PaddingValues(horizontal = 32.dp),
+            modifier = Modifier.fillMaxSize()
+        ) { page ->
+            val movie = uiState.popularMovies[page]
+            Card(
+                Modifier
+                    .graphicsLayer {
+                        // Calculate the absolute offset for the current page from the
+                        // scroll position. We use the absolute value which allows us to mirror
+                        // any effects for both directions
+                        val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
 
-@Composable
-fun PopularMoviesHorizontalPager(movies: List<Movie>) {
-    HorizontalPager(
-        count = movies.size,
-        contentPadding = PaddingValues(horizontal = 32.dp),
-        modifier = Modifier.fillMaxSize()
-    ) { page ->
-        val movie = movies[page]
-        Card(
-            Modifier
-                .graphicsLayer {
-                    // Calculate the absolute offset for the current page from the
-                    // scroll position. We use the absolute value which allows us to mirror
-                    // any effects for both directions
-                    val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
+                        // We animate the scaleX + scaleY, between 85% and 100%
+                        lerp(
+                            start = 0.90f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        ).also { scale ->
+                            scaleX = scale
+                            scaleY = scale
+                        }
 
-                    // We animate the scaleX + scaleY, between 85% and 100%
-                    lerp(
-                        start = 0.90f,
-                        stop = 1f,
-                        fraction = 1f - pageOffset.coerceIn(0f, 1f)
-                    ).also { scale ->
-                        scaleX = scale
-                        scaleY = scale
+                        // We animate the alpha, between 50% and 100%
+                        alpha = lerp(
+                            start = 0.5f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        )
                     }
-
-                    // We animate the alpha, between 50% and 100%
-                    alpha = lerp(
-                        start = 0.5f,
-                        stop = 1f,
-                        fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                    .fillMaxWidth()
+                    .aspectRatio(1.7f)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clip(MaterialTheme.shapes.small)
+                ) {
+                    MoviePhoto(poster = movie.backdrop?.original)
+                    Text(
+                        text = movie.title,
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 14.sp,
+                        modifier = Modifier
+                            .align(BottomStart)
+                            .padding(16.dp)
+                            .background(
+                                Color.Black.copy(alpha = 0.6f),
+                                shape = RoundedCornerShape(8.dp)
+                            )
+                            .padding(8.dp)
                     )
                 }
-                .fillMaxWidth()
-                .aspectRatio(1.7f)
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .clip(MaterialTheme.shapes.small)
-            ) {
-                MoviePhoto(poster = movie.backdrop?.original)
-                Text(
-                    text = movie.title,
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 14.sp,
-                    modifier = Modifier
-                        .align(BottomStart)
-                        .padding(16.dp)
-                        .background(
-                            Color.Black.copy(alpha = 0.6f),
-                            shape = RoundedCornerShape(8.dp)
-                        )
-                        .padding(8.dp)
-                )
             }
         }
     }
 }
+
