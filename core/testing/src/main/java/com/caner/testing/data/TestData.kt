8659cdc2b6487160345760a525a27bf9e5ca7952
canergulgec <canergulgec@gmail.com>
2025-08-04 10:51:21 +0300

    Refactor: Remove unused `MovieDto` and `MovieListResponse` from TestData
    - Delete `createMovieResponse` and `createMovieResponseItemList` functions.
    - Remove imports for `MovieDto` and `MovieListResponse`.

--- core/testing/src/main/java/com/caner/testing/data/TestData.kt
+++ core/testing/src/main/java/com/caner/testing/data/TestData.kt
@@ -3,8 +3,6 @@ package com.caner.testing.data
 import com.caner.domain.model.Movie
 import com.caner.domain.model.MovieImage
 import com.caner.domain.model.MovieList
-import com.caner.model.remote.MovieDto
-import com.caner.model.remote.MovieListResponse
 
 object TestData {
     private fun createMovie(movieId: Int = 1, title: String = "Test Movie", voteAverage: Double = 8.5) =
@@ -25,26 +23,5 @@ object TestData {
             total = movies.size,
             page = 0,
             movies = movies
-        )
-
-    fun createMovieResponse() =
-        MovieListResponse(
-            total = 15,
-            page = 2,
-            results = createMovieResponseItemList()
-        )
-
-    private fun createMovieResponseItemList() =
-        listOf(
-            MovieDto(
-                id = 1,
-                popularity = 0.0,
-                posterPath = "",
-                backdropPath = "",
-                title = "Test Movie 1",
-                voteAverage = 8.5,
-                overview = "",
-                releaseDate = ""
-            )
         )
 }
